1befff483089c638170079bb0250681582b854d0
dmazepa <dmazepa@speroteck.com>
2022-01-13 14:56:17 +0200

    init5

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
-chromepath = os.environ.get("CHROMEDRIVER_PATH")
-print(chromepath)
-ser = Service(chromepath)
-
+# chromepath = os.environ.get("CHROMEDRIVER_PATH")
+# print(chromepath)
+# ser = Service(chromepath)
+#
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(service=ser, options=chrome_options)
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(options=chrome_options)
 
 driver.get("http://www.python.org")
 assert "Python" in driver.title
